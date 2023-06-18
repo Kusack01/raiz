3c83f69acd0b05c6a60aaafd7b626fd3bd5a8686
SebvstiannMedina <seba.medina@duocuc.cl>
2023-06-18 19:44:34 -0400

    a

--- botanica/urls.py
+++ botanica/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('AgregarEditarEliminar/',agregar_editar_eliminar,name="agregar_editar_eliminar"),
     path('Acercade/',acercade,name="acercade"),
     path('MetododePago/',metodo_pago,name="metodo_pago"),
-    path('cuenta/editar/', views.editar_cuenta, name='editar cuenta'),
+    path('cuenta_editar/', views.editar_cuenta, name='editar cuenta'),
     path('codigorecuperacion/',codigo_recuperacion,name="codigo_recuperacion")
 
 ]
