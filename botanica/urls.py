199fe47dff8f9f3349ad8ed3fe0ff3409ffcac3a
Kusack01 <MA.reyesv@duocuc.cl>
2023-06-18 19:42:53 -0400

    cosas wey

--- botanica/urls.py
+++ botanica/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from .views import raiz2,venta,recu,recuperar,terminoycondicion,masetaterra,maceterobio
 from .views import macetero1,macetabonsai,macetaexter,ini,crear_usu,crear_admi,contacto,compralista
 from .views import carrito,carrito_terracota,carrito_bonsai,carrito_biodegradable,agregar_editar_eliminar
-from .views import acercade,metodo_pago,editar_cuenta,codigo_recuperacion
+from .views import acercade,metodo_pago,editarcuenta,codigo_recuperacion
 from .import views 
 
 urlpatterns = [
@@ -32,7 +32,7 @@ urlpatterns = [
     path('AgregarEditarEliminar/',agregar_editar_eliminar,name="agregar_editar_eliminar"),
     path('Acercade/',acercade,name="acercade"),
     path('MetododePago/',metodo_pago,name="metodo_pago"),
-    path('cuenta/editar/', views.editar_cuenta, name='editar cuenta'),
+    path('cuenta/editar/',editarcuenta, name='editar cuenta'),
     path('codigorecuperacion/',codigo_recuperacion,name="codigo_recuperacion")
 
 ]
