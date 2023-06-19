735adb7bbf4f575663aaa78a31ab6187927afab2
Kusack01 <MA.reyesv@duocuc.cl>
2023-06-18 22:47:52 -0400

    cosa

--- botanica/forms.py
+++ botanica/forms.py
@@ -1,3 +1,12 @@
 from django import forms
 from django.forms import ModelForm
-from .models import 
+from .models import Producto,Usuario
+
+class ProductoForm(ModelForm):
+    class meta:
+        model:Producto
+        fields=['idProducto','nombre','precio','stock','idCategoria']
+
+class UsuarioForm(ModelForm):
+    class meta:
+        model:Usuario
